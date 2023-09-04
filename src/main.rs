4e7717e8f994bcf40ee400878924a8d3645dc7e5
adam-bates <adam@adambates.ca>
2023-09-04 14:44:40 -0400

    Started reading source from files

--- src/main.rs
+++ src/main.rs
@@ -6,45 +6,13 @@ use ferrum_compiler::syntax::*;
 use ferrum_compiler::type_resolver::*;
 
 use std::collections::HashMap;
+use std::env;
+use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
 
 fn main() -> ferrum_compiler::result::Result {
-    let mut local_packages = HashMap::new();
-
-    local_packages.insert(
-        SourcePackageName("utils".into()),
-        Arc::new(Mutex::new(FeSourcePackage::File(FeSourceFile {
-            name: SourcePackageName("utils".into()),
-            path: "src/utils.fe".into(),
-            content: r#"
-            use ::fe::print
-
-            pub fn hello(name: &String)
-                print("Hello {name}!")
-            ;
-         "#
-            .into(),
-        }))),
-    );
-
-    let source = Arc::new(Mutex::new(FeSourcePackage::Dir(FeSourceDir {
-        name: SourcePackageName("src".into()),
-        path: "src/".into(),
-        entry_file: FeSourceFile {
-            name: SourcePackageName("_main".into()),
-            path: "src/_main.fe".into(),
-            content: r#"
-            use ./utils::hello
-
-            pub fn main()
-                hello(&"Adam")
-            ;
-      "#
-            .into(),
-        },
-        local_packages,
-        // local_packages: HashMap::new(),
-    })));
+    // TODO: Make code available in compiler lib to do this
+    let source = read_source()?;
 
     let tokens = Arc::new(Mutex::new(FeLexer::scan_package(source)?));
     // dbg!(&tokens);
@@ -68,4 +36,143 @@ fn main() -> ferrum_compiler::result::Result {
     }
 
     return Ok(());
+}
+
+fn read_source() -> ferrum_compiler::result::Result<Arc<Mutex<FeSourcePackage>>> {
+    fn build_dir_pkg(
+        dir: PathBuf,
+        name: SourcePackageName,
+    ) -> ferrum_compiler::result::Result<FeSourcePackage> {
+        let src_dir_entries = dir.read_dir()?;
+        let mut local_packages = HashMap::new();
+
+        for pkg in src_dir_entries {
+            let pkg = pkg?;
+
+            let name = SourcePackageName(
+                pkg.path()
+                    .file_stem()
+                    .unwrap()
+                    .to_os_string()
+                    .into_string()
+                    .unwrap()
+                    .into(),
+            );
+
+            if name.0.as_ref() == "_pkg" {
+                continue;
+            }
+
+            if pkg.file_type()?.is_dir() {
+                let pkg = build_dir_pkg(pkg.path(), name.clone())?;
+
+                local_packages.insert(name, Arc::new(Mutex::new(pkg)));
+            } else {
+                let path = pkg.path().into_os_string().into_string().unwrap().into();
+                let content = std::fs::read_to_string(&path)?.into();
+
+                let pkg = FeSourcePackage::File(FeSourceFile {
+                    name: name.clone(),
+                    path,
+                    content,
+                });
+
+                local_packages.insert(name, Arc::new(Mutex::new(pkg)));
+            }
+        }
+
+        let pkg = dir.join("_pkg.fe");
+        if !pkg.is_file() {
+            panic!("Expected package {:?} to contain a '_pkg.fe' file", dir);
+        }
+        let pkg_content = std::fs::read_to_string(&pkg)?.into();
+
+        let entry_file = FeSourceFile {
+            name: SourcePackageName("_pkg".into()),
+            path: pkg,
+            content: pkg_content,
+        };
+
+        return Ok(FeSourcePackage::Dir(FeSourceDir {
+            name,
+            path: dir,
+            entry_file,
+            local_packages,
+        }));
+    }
+
+    if env::args().count() != 2 {
+        panic!("Expected 1 arg: path to the root of the project directory");
+    }
+
+    let root_dir = env::args().last().unwrap();
+    let root_dir = PathBuf::from(root_dir);
+    if !root_dir.is_dir() {
+        panic!(
+            "Expected arg 1 to be a directory to the project root, found {:?}",
+            root_dir
+        );
+    }
+
+    let src_dir = root_dir.join("src");
+    if !src_dir.is_dir() {
+        panic!("Expected the project root to contain a 'src' directory");
+    }
+
+    let src_dir_entries = src_dir.read_dir()?;
+    let mut local_packages = HashMap::new();
+
+    for pkg in src_dir_entries {
+        let pkg = pkg?;
+
+        let name = SourcePackageName(
+            pkg.path()
+                .file_stem()
+                .unwrap()
+                .to_os_string()
+                .into_string()
+                .unwrap()
+                .into(),
+        );
+
+        if name.0.as_ref() == "_main" {
+            continue;
+        }
+
+        if pkg.file_type()?.is_dir() {
+            let pkg = build_dir_pkg(pkg.path(), name.clone())?;
+
+            local_packages.insert(name, Arc::new(Mutex::new(pkg)));
+        } else {
+            let path = pkg.path().into_os_string().into_string().unwrap().into();
+            let content = std::fs::read_to_string(&path)?.into();
+
+            let pkg = FeSourcePackage::File(FeSourceFile {
+                name: name.clone(),
+                path,
+                content,
+            });
+
+            local_packages.insert(name, Arc::new(Mutex::new(pkg)));
+        }
+    }
+
+    let main = src_dir.join("_main.fe");
+    if !main.is_file() {
+        panic!("Expected project root to contain a '_main.fe' file");
+    }
+    let main_content = std::fs::read_to_string(main)?;
+
+    let source = FeSourcePackage::Dir(FeSourceDir {
+        name: SourcePackageName("src".into()),
+        path: "src/".into(),
+        entry_file: FeSourceFile {
+            name: SourcePackageName("_main".into()),
+            path: "src/_main.fe".into(),
+            content: main_content.into(),
+        },
+        local_packages,
+    });
+
+    return Ok(Arc::new(Mutex::new(source)));
 }
